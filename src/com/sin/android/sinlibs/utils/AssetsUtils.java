d5093c26a2d22e0dfa71596991b95fdb394055d7
RobinTang <sintrb@gmail.com>
2014-01-02 10:24:37 +0800

    为AssetsUtils增加注释

--- src/com/sin/android/sinlibs/utils/AssetsUtils.java
+++ src/com/sin/android/sinlibs/utils/AssetsUtils.java
@@ -5,7 +5,19 @@ import java.io.InputStreamReader;
 
 import android.content.Context;
 
+/**
+ * Assets文件操作封装
+ * 
+ * @author RobinTang
+ *         2014-1-2
+ */
 public class AssetsUtils {
+	/**
+	 * 读取文本文件
+	 * @param context	上下文
+	 * @param fileName	文件名
+	 * @return	文本内容
+	 */
 	public static String readAssetTxt(Context context, String fileName) {
 		try {
 			InputStreamReader ir = new InputStreamReader(context.getResources().getAssets().open(fileName));
