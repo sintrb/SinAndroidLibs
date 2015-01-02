4f47b26db5547c1ad0e48f06a58308d691735c47
RobinTang <sintrb@gmail.com>
2015-01-02 15:53:56 +0800

    read drawable

--- src/com/sin/android/sinlibs/utils/AssetsUtils.java
+++ src/com/sin/android/sinlibs/utils/AssetsUtils.java
@@ -4,19 +4,23 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 import android.content.Context;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 
 /**
  * Assets文件操作封装
  * 
- * @author RobinTang
- *         2014-1-2
+ * @author RobinTang 2014-1-2
  */
 public class AssetsUtils {
 	/**
 	 * 读取文本文件
-	 * @param context	上下文
-	 * @param fileName	文件名
-	 * @return	文本内容
+	 * 
+	 * @param context
+	 *            上下文
+	 * @param fileName
+	 *            文件名
+	 * @return 文本内容
 	 */
 	public static String readAssetTxt(Context context, String fileName) {
 		try {
@@ -31,6 +35,26 @@ public class AssetsUtils {
 			br.close();
 			ir.close();
 			return sb.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * 读取图片文件
+	 * 
+	 * @param context
+	 *            上下文
+	 * @param fileName
+	 *            文件名
+	 * @return 文本内容
+	 */
+	public static Drawable readAssetPng(Context context, String fileName) {
+		try {
+			@SuppressWarnings("deprecation")
+			BitmapDrawable bd = new BitmapDrawable(context.getResources().getAssets().open(fileName));
+			return bd;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
