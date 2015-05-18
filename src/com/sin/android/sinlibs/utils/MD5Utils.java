b323b1163f1ffd91503eb12b2d14a2dc4eeb4775
RobinTang <sintrb@gmail.com>
2015-05-18 10:41:15 +0800

    md5

---
+++ src/com/sin/android/sinlibs/utils/MD5Utils.java
@@ -0,0 +1,39 @@
+package com.sin.android.sinlibs.utils;
+
+import java.security.MessageDigest;
+
+/**
+ * MD5 Utils
+ * 
+ * @author Robin 2015-4-20
+ * 
+ */
+public class MD5Utils {
+	public static String calcMD5(String str) {
+		MessageDigest md5 = null;
+		try {
+			md5 = MessageDigest.getInstance("MD5");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "";
+		}
+
+		char[] charArray = str.toCharArray();
+		byte[] byteArray = new byte[charArray.length];
+
+		for (int i = 0; i < charArray.length; i++) {
+			byteArray[i] = (byte) charArray[i];
+		}
+		byte[] md5Bytes = md5.digest(byteArray);
+
+		StringBuffer hexValue = new StringBuffer();
+		for (int i = 0; i < md5Bytes.length; i++) {
+			int val = ((int) md5Bytes[i]) & 0xff;
+			if (val < 16) {
+				hexValue.append("0");
+			}
+			hexValue.append(Integer.toHexString(val));
+		}
+		return hexValue.toString();
+	}
+}
