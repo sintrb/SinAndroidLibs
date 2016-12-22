90786018cc81fd9e08c13c5fdd8247d40dfb1bb5
sintrb <sintrb@gmail.com>
2016-12-23 01:13:15 +0800

    mutil attr eval

--- src/com/sin/android/sinlibs/tagtemplate/ExpressionEngine.java
+++ src/com/sin/android/sinlibs/tagtemplate/ExpressionEngine.java
@@ -6,14 +6,30 @@ import java.lang.reflect.Method;
 
 public class ExpressionEngine {
 	public Object eval(Object model, String exp) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
-		if (exp.startsWith(".a:")) {
-			return evalAttribute(model, exp.substring(3));
-		} else if (exp.startsWith(".m:")) {
-			return evalMethod(model, exp.substring(3));
-		} else if (exp.startsWith(".")) {
-			return evalAttribute(model, exp.substring(1));
+		Object res = null;
+		String nexp = null;
+        String[] atbs = exp.split("\\.");
+		for(String s: atbs){
+			nexp = s;
+			if(nexp.length()>0){
+				if (nexp.startsWith("a:")) {
+					res = model = evalAttribute(model, nexp.substring(2));
+				} else if (nexp.startsWith("m:")) {
+					res = model = evalMethod(model, nexp.substring(2));
+				} else{
+					res = model = evalAttribute(model, nexp);
+				}
+			}
 		}
-		throw new IllegalArgumentException("unknown expression: " + exp);
+		return res;
+//		if (exp.startsWith(".a:")) {
+//			return evalAttribute(model, exp.substring(3));
+//		} else if (exp.startsWith(".m:")) {
+//			return evalMethod(model, exp.substring(3));
+//		} else if (exp.startsWith(".")) {
+//			return evalAttribute(model, exp.substring(1));
+//		}
+//		throw new IllegalArgumentException("unknown expression: " + exp);
 	}
 
 	public Object evalAttribute(Object model, String att) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
