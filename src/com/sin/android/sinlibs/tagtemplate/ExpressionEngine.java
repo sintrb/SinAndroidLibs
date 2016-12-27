6667fdeb81d2a66048c29fbfbcac7f30822cbc40
sintrb <sintrb@gmail.com>
2016-12-27 23:02:40 +0800

    handle model null

--- src/com/sin/android/sinlibs/tagtemplate/ExpressionEngine.java
+++ src/com/sin/android/sinlibs/tagtemplate/ExpressionEngine.java
@@ -5,23 +5,23 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class ExpressionEngine {
-	public Object eval(Object model, String exp) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
-		Object res = null;
-		String nexp = null;
+    public Object eval(Object model, String exp) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Object res = null;
+        String nexp = null;
         String[] atbs = exp.split("\\.");
-		for(String s: atbs){
-			nexp = s;
-			if(nexp.length()>0){
-				if (nexp.startsWith("a:")) {
-					res = model = evalAttribute(model, nexp.substring(2));
-				} else if (nexp.startsWith("m:")) {
-					res = model = evalMethod(model, nexp.substring(2));
-				} else{
-					res = model = evalAttribute(model, nexp);
-				}
-			}
-		}
-		return res;
+        for (String s : atbs) {
+            nexp = s;
+            if (nexp.length() > 0 && model != null) {
+                if (nexp.startsWith("a:")) {
+                    res = model = evalAttribute(model, nexp.substring(2));
+                } else if (nexp.startsWith("m:")) {
+                    res = model = evalMethod(model, nexp.substring(2));
+                } else {
+                    res = model = evalAttribute(model, nexp);
+                }
+            }
+        }
+        return res;
 //		if (exp.startsWith(".a:")) {
 //			return evalAttribute(model, exp.substring(3));
 //		} else if (exp.startsWith(".m:")) {
@@ -30,17 +30,17 @@ public class ExpressionEngine {
 //			return evalAttribute(model, exp.substring(1));
 //		}
 //		throw new IllegalArgumentException("unknown expression: " + exp);
-	}
+    }
 
-	public Object evalAttribute(Object model, String att) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
-		Class<?> clz = model.getClass();
-		Field field = clz.getField(att);
-		return field.get(model);
-	}
+    public Object evalAttribute(Object model, String att) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+        Class<?> clz = model.getClass();
+        Field field = clz.getField(att);
+        return field.get(model);
+    }
 
-	public Object evalMethod(Object model, String met) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-		Class<?> clz = model.getClass();
-		Method method = clz.getMethod(met);
-		return method.invoke(model);
-	}
+    public Object evalMethod(Object model, String met) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        Class<?> clz = model.getClass();
+        Method method = clz.getMethod(met);
+        return method.invoke(model);
+    }
 }
